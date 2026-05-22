731e64666558713f3f3c2dbaebafb15ede57a582
aakashop1995 <aakashop1995@gmail.com>
2026-05-22 15:46:32 +0530

    Update main.py

--- Pothole_Updated/pothole_car/main.py
+++ Pothole_Updated/pothole_car/main.py
@@ -226,7 +226,7 @@ if __name__ == '__main__':
         app.run(
             host='0.0.0.0',
             port=5000,
-            threaded=False,
+            threaded=True,
             debug=False,
             use_reloader=False
         )
